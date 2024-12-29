ae5ad9bf6ae27d444f20ec3e2bdd1bab79fcd2e2
alpha <amitk.vishwa1633@gmail.com>
2024-12-30 00:30:15 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+
+import http from "http";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
